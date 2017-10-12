1b612e42570e8bd07b0562ef4fc076a96cc6f788
iracanyes <k.iracanye@gmail.com>
2017-10-12 20:48:26 +0200

    structure Theme

---
+++ footer.php
@@ -0,0 +1,7 @@
+/*
+    Permet le chargement du footer du corps de la page, les fichiers javascripts.
+    Le chargement des fichiers JS permet aussi de créer la barre administrateur côté Admin
+*/
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="<?php bloginfo( 'pingback_url' ) ?>">
+
+    <?php wp_head(); ?>
+</head>
+
